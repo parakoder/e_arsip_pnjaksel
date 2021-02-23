5eee08d513301760f472b0637a86a33c1fd7bfb6
Andika Eka Putra <andika.ekaputra@dejavuexpress.com>
2021-02-23 23:02:48 +0700

    tes

--- src/pages/about/login/Login.js
+++ src/pages/about/login/Login.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Login = () => {
-	return <div>Login Pages</div>;
+	return <div>Login Pages
+		<div>
+			<button
+			
+			>klik login</button>
+		</div>	
+	</div>;
 };
 
 export default Login;
