dcc61a9e4fa42ac0a9188b89df17a535606aee81
Twin Edo <venganzdektor26@gmail.com>
2021-03-28 17:51:50 +0700

    add graph stat all, ubah kolom log 10 di page Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,6 +7,7 @@ import { Bar } from 'react-chartjs-2';
 import {
 	GraphPerYearHandler,
 	GraphYearsHandler,
+	GraphStat,
 } from '../../configs/handler/GraphHandler';
 import { GetLogActivity } from '../../configs/handler/HomeHandler';
 import { GetArsipSum } from '../../configs/handler/ArsipHandler';
@@ -52,7 +53,8 @@ function Home(props) {
 		getLogDt();
 	}, []);
 
-	const [year, setYear] = useState(2021);
+	const [year, setYear] = useState(new Date().getFullYear());
+	const [yearAll, setYearAll] = useState(new Date().getFullYear());
 
 	const [stateGraph, setStateGraph] = useState({
 		labels: [
@@ -72,17 +74,17 @@ function Home(props) {
 		datasets: [
 			{
 				label: 'Pidana',
-				backgroundColor: 'rgba(248,224,134,1)',
-				borderColor: 'rgba(248,224,134,1)',
+				backgroundColor: 'rgba(207,33,42,1)',
+				borderColor: 'rgba(207,33,42,1)',
 				borderWidth: 1,
-				data: [65, 59, 80, 81, 56, 20, 25, 70, 90, 120, 10, 20],
+				data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 			},
 			{
 				label: 'Perdata',
 				backgroundColor: 'rgba(118,142,124,1)',
 				borderColor: 'rgba(118,142,124,1)',
 				borderWidth: 1,
-				data: [65, 59, 80, 81, 56, 20, 25, 70, 90, 120, 10, 20],
+				data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 			},
 		],
 	});
@@ -99,8 +101,8 @@ function Home(props) {
 						datasets: [
 							{
 								label: 'Pidana',
-								backgroundColor: 'rgba(248,224,134,1)',
-								borderColor: 'rgba(248,224,134,1)',
+								backgroundColor: 'rgba(207,33,42,1)',
+								borderColor: 'rgba(207,33,42,1)',
 								borderWidth: 1,
 								data: res.data.dataPidana,
 							},
@@ -120,6 +122,54 @@ function Home(props) {
 			});
 		return () => {};
 	}, [year]);
+
+	const [allGraph, setAllGraph] = useState({
+		labels: ['Pidana', 'Perdata', 'Hukum', 'Kepegawaian', 'Umum dan Keuangan'],
+		datasets: [
+			{
+				label: '',
+				backgroundColor: [
+					'rgba(207,33,42,1)',
+					'rgba(118,142,124,1)',
+					'rgba(57,47,8,1)',
+					'rgba(13,70,27,1)',
+					'rgba(248,224,134,1)',
+				],
+				borderWidth: 1,
+				data: [0, 0, 0, 0, 0],
+			},
+		],
+	});
+
+	useEffect(() => {
+		GraphStat(yearAll)
+			.then((res) => {
+				console.log('res graph all', res.data);
+				if (res.status === 200) {
+					setAllGraph({
+						...allGraph,
+						datasets: [
+							{
+								label: '',
+								backgroundColor: [
+									'rgba(207,33,42,1)',
+									'rgba(118,142,124,1)',
+									'rgba(57,47,8,1)',
+									'rgba(13,70,27,1)',
+									'rgba(248,224,134,1)',
+								],
+								borderWidth: 1,
+								data: res.data.data,
+							},
+						],
+					});
+				}
+			})
+			.catch((err) => {
+				console.log('err graph all', err);
+			});
+		return () => {};
+	}, [yearAll]);
 
 	useEffect(() => {
 		GetArsipSum()
@@ -217,64 +267,123 @@ function Home(props) {
 							</div>
 						</div>
 					</div>
-
-					{/* <Gap width={30} height={30} /> */}
 				</div>
-				{/* <Gap height={22} /> */}
-				<div className='wrapperSection'>
-					<p className='titleSection'>Log Aktifitas Terakhir</p>
-					{/* <div className='dateSection'>
+				<div className='wrapperGraph row '>
+					<div className='col-xl-6 col-lg-6 col-md-12 col-sm-12 mb-20px'>
+						<div>
+							<div className='wrapperSection'>
+								<p className='titleSection'>Klasifikasi Perkara</p>
+							</div>
+							<div className='graph'>
+								<div className='graph-header'>
+									<div className='title'></div>
+									<Dropdown
+										options={yearsArray}
+										onChange={(e) => setYearAll(e.value)}
+										value={yearsArray.find(
+											(o) => o === new Date().getFullYear().toString()
+										)}
+										placeholder='Tahun'
+										controlClassName='classes'
+										menuClassName='classes'
+									/>
+								</div>
+								<div
+									className='graph-content'
+									style={{ display: 'flex', flexDirection: 'column' }}
+								>
+									<Bar
+										data={allGraph}
+										options={{
+											legend: {
+												display: false,
+												position: 'bottom',
+											},
+										}}
+									/>
+									<div>
+										<div className='wrapperSection'>
+											<p className='titleSection'>Ket</p>
+										</div>
+										<div
+											style={{
+												display: 'flex',
+												flexDirection: 'row',
+												flexWrap: 'wrap',
+											}}
+										>
+											<div style={{ display: 'flex', flex: 1 }}>Pidana</div>
+											<div style={{ display: 'flex', flex: 1 }}>Perdata</div>
+											<div style={{ display: 'flex', flex: 1 }}>Hukum</div>
+											<div style={{ display: 'flex', flex: 1 }}>
+												Kepegawaian
+											</div>
+											<div style={{ display: 'flex', flex: 1 }}>
+												Umum dan Keuangan
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='col-xl-6 col-lg-6 col-md-12 col-sm-12 mb-20px'>
+						<div className='wrapperSection'>
+							<p className='titleSection'>Log Aktifitas Terakhir</p>
+							{/* <div className='dateSection'>
 						<p className='txtLastUpdate'>Last Update:</p>
 						<Gap width={10} />
 						<p className='txtValueLastUpdate'>22 Februari 2021</p>
 					</div> */}
-				</div>
+						</div>
 
-				<div className='c-table-main'>
-					<table className='table-main'>
-						<thead className='table-main-thead'>
-							<tr>
-								<th className='table-main-th' style={{ width: '5%' }}>
+						<div className='c-table-main'>
+							<table className='table-main'>
+								<thead className='table-main-thead'>
+									<tr>
+										{/* <th className='table-main-th' style={{ width: '5%' }}>
 									No.
-								</th>
-								<th className='table-main-th' style={{ width: '30%' }}>
-									User
-								</th>
-								<th className='table-main-th' style={{ width: '20%' }}>
+								</th> */}
+										<th className='table-main-th' style={{ width: '30%' }}>
+											User
+										</th>
+										{/* <th className='table-main-th' style={{ width: '20%' }}>
 									Tanggal
 								</th>
 								<th className='table-main-th' style={{ width: '15%' }}>
 									Waktu
-								</th>
-								<th className='table-main-th' style={{ width: '30%' }}>
-									Aktifitas
-								</th>
-							</tr>
-						</thead>
+								</th> */}
+										<th className='table-main-th' style={{ width: '30%' }}>
+											Aktifitas
+										</th>
+									</tr>
+								</thead>
 
-						<tbody className='table-main-tbody'>
-							{dtTableHome === null
-								? null
-								: dtTableHome.map((dt, i) => (
-										<tr key={i} className='table-main-nth-child'>
-											<td className='table-main-td'>{i + 1}</td>
-											<td
-												className='table-main-td'
-												style={{ textAlign: 'left' }}
-											>
-												{dt.user}
-											</td>
-											<td className='table-main-td'>
+								<tbody className='table-main-tbody'>
+									{dtTableHome === null
+										? null
+										: dtTableHome.map((dt, i) => (
+												<tr key={i} className='table-main-nth-child'>
+													{/* <td className='table-main-td'>{i + 1}</td> */}
+													<td
+														className='table-main-td'
+														style={{ textAlign: 'left' }}
+													>
+														{dt.user}
+													</td>
+													{/* <td className='table-main-td'>
 												{moment(dt.tanggal).utc().format('yyyy-MM-DD')}
 											</td>
 											<td className='table-main-td'>
 												{moment(dt.waktu, 'h:mm ').format('h:mm ')}
-											</td>
-											<td className='table-main-td'>{dt.aktifitas}</td>
-										</tr>
-								  ))}
-						</tbody>
-					</table>
+											</td> */}
+													<td className='table-main-td'>{dt.aktifitas}</td>
+												</tr>
+										  ))}
+								</tbody>
+							</table>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
